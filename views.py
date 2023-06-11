adb64eec4804a8c4c7a70a7518682a41f00918f9
George Vatalis <giwrgos.vat@gmail.com>
2023-06-11 17:57:03 +0300

    changes on the footer section
    add of social media icons

--- views.py
+++ views.py
@@ -22,7 +22,9 @@ def process_string():
 
     
     return render_template('toxicity.html', result1=output1, result2=output2)
+
 # Serve static files
 @views.route('/static/<path:filename>')
 def serve_static(filename):
     return views.send_static_file(filename)
+
